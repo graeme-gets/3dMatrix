d2589f89ba2d175d51e69a055e93eeda99070ec9
Graeme Gets <graeme@athousandprojects.com>
2024-02-06 15:29:46 +0000

    Added PrintMatrixList

--- main.py
+++ main.py
@@ -2,11 +2,9 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import numpy
-from numpy import matrix
 
 Matrix3D = []
-
+MatrixNull = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
 def PrintMatrix(matrix):
     # Hpw many arrays to print
@@ -20,34 +18,44 @@ def formatMatrixRow(row, width):
             f"{str.center(str(row[2]), width, ' ')} |")
 
 
+# https://stackoverflow.com/questions/434287/how-to-iterate-over-a-list-in-chunks
+def chunker(seq, size):
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
+
 def PrintMatrixList(matrixLst, cols):
-    noofMatrix = len(matrixLst)
-    for i in range(0, noofMatrix):
+    for group in chunker(matrixLst, cols):
         s = ""
-        for col in range(0, cols):
-            m = matrixLst[i]
-            s = s + formatMatrixRow(m[col],3) + "   "
-        print(s)
+        for row in range(3):
+            for m in range(cols):
+                if m >= len(group):
+                    s = s + f"{formatMatrixRow(MatrixNull[row], 3)}\t"
+                else:
+                    s = s + f"{formatMatrixRow(group[m][row],3)}\t"
+            print(s)
+            s = ""
+        print("")
 
 
 def EnteredMatrix():
-    for i in range(0, 3):
-        row = input(f"Enter in row {i + 1} : ")
-        row = row.replace(" ", "")
-        rowSplit = row.split(',')
-        if len(rowSplit) != 3:
-            print(f"Syntax Error : {rowSplit}")
-            exit()
-        rowArray = []
-        for j in range(0, 3):
-            rowArray.append(int(rowSplit[j]))
-        Matrix3D.append(rowArray)
+    # for i in range(0, 3):
+    #     row = input(f"Enter in row {i + 1} : ")
+    #     row = row.replace(" ", "")
+    #     rowSplit = row.split(',')
+    #     if len(rowSplit) != 3:
+    #         print(f"Syntax Error : {rowSplit}")
+    #         exit()
+    #     rowArray = []
+    #     for j in range(0, 3):
+    #         rowArray.append(int(rowSplit[j]))
+    #     Matrix3D.append(rowArray)
 
-    lst = []
-    lst.append(Matrix3D)
-    lst.append(Matrix3D.reverse())
-    PrintMatrix(Matrix3D)
-    PrintMatrixList(lst,2)
+    lst = [ ]
+    lst.append([[1, 1, 1], [2, 2, 2], [3, 3, 3]])
+    lst.append([[4, 4, 4], [5, 5, 5], [6, 7, 7]])
+    lst.append([[7, 7, 7], [8, 8, 8], [9, 9, 9]])
+    #PrintMatrix(Matrix3D)
+    PrintMatrixList(lst, 2)
 
 
 # Press the green button in the gutter to run the script.
